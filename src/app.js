96d04ff32784571169ca598bce3a7b0a68c3ac63
jeffersontdm <jeffersontavares1611@gmail.com>
2022-11-25 20:03:58 -0300

    remove useless files

--- src/app.js
+++
@@ -1,7 +0,0 @@
-const {config} = angular.module("App" ["ngRoute"]);
-
-config(($routeProvider) => {
-  $routeProvider
-    .when("/", {templateUrl: "./app/app.component.html"})
-    .otherwise({redirectTo: "/"})
-});
